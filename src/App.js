da7eb96569e209aea48e74173e3c967b57555a11
Nasir <nasir-islam@hotmail.com>
2022-09-26 14:37:48 +0100

    create Face component and compartmentalise it

--- src/App.js
+++ src/App.js
@@ -2,42 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import ReactDom from 'react-dom'
-import {arc} from 'd3'
+import { Face } from './components/Face';
 
-console.log(arc)
+
 function App() {
-
-  const width = 960;
-  const height = 500;
-  const centerX = width/2;
-  const centerY = height/2;
-  const strokeWidth = 10;
-  const eyeOffsetX = 90;
-  const eyeOffsetY = 100;
-  const eyeRadius = 50;
   
   return (
     <div className="App">
-     <svg width={width} height={height}>
-      <circle
-      cx={centerX}
-      cy={centerY}
-      r={centerY-strokeWidth/2}
-      fill="yellow"
-      stroke='black'
-      stroke-width={strokeWidth}
-      />
-      <circle
-      cx={centerX - eyeOffsetX}
-      cy={centerY - eyeOffsetY}
-      r={eyeRadius}
-      />
-       <circle
-      cx={centerX + eyeOffsetX}
-      cy={centerY - eyeOffsetY}
-      r={eyeRadius}
-      />
-     </svg>
+      {/* <Face/> */}
     </div>
   );
 }
---
+++ src/components/Face.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { arc } from 'd3'
+
+export const Face = () => {
+
+    const width = 960;
+  const height = 500;
+  const centerX = width/2;
+  const centerY = height/2;
+  const strokeWidth = 10;
+  const eyeOffsetX = 90;
+  const eyeOffsetY = 100;
+  const eyeRadius = 50;
+  const mouthWidth = 20;
+  const mouthRadius = 140;
+
+  const mouthArc = arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius + mouthWidth)
+    .startAngle(Math.PI / 2)
+    .endAngle(Math.PI * 3 / 2);
+
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        <circle
+        r={centerY-strokeWidth/2}
+        fill="yellow"
+        stroke='black'
+        strokeWidth={strokeWidth}
+        />
+        <circle
+        cx={- eyeOffsetX}
+        cy={- eyeOffsetY}
+        r={eyeRadius}
+        />
+        <circle
+        cx={eyeOffsetX}
+        cy={- eyeOffsetY}
+        r={eyeRadius}
+        />
+        <path d={mouthArc()}/>
+      </g>
+     </svg>
+  )
+}

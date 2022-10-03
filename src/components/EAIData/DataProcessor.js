3ad63dfd9375ab2c01ceb0d5152fb8ac0fc63e01
Nasir <nasir-islam@hotmail.com>
2022-10-03 22:29:42 +0100

    add search/highlight functionality

--- src/components/EAIData/DataProcessor.js
+++ src/components/EAIData/DataProcessor.js
@@ -1,131 +1,120 @@
-import {useState} from 'react'
-import {read, utils} from 'xlsx'
-import { csvParse } from 'd3';
+import { useState } from "react";
+import { read, utils } from "xlsx";
+import { csvParse } from "d3";
 // import {createReadStream} from 'fs'
 
-import { parse } from 'papaparse'
-
-
-
+import { parse } from "papaparse";
 
 export const DataProcessor = () => {
+  const csvToJson = (csvData) => {
+    // initialise the JSON object
+    const data = {
+      name: "Education AI Curriculum",
+      children: [],
+    };
 
-    const csvToJson = (csvData) => {
-        // initialise the JSON object 
-        const data = {
-            name: "Education AI Curriculum",
-            children: []
-        };
-
-        for (const row of csvData) {
-
-
-          const newCat = data;
-            if (!newCat.children.find(a => a.name === row['Parent Category'])) {
-                newCat.children.push({
-                    name: row['Parent Category'],
-                    children: []
-                })
-            }
-
-
-
-
-            const cat = newCat.children.find(a => a.name === row['Parent Category']);
-            if (!cat.children.find(a => a.name === row['Computational Concepts'])) {
-                cat.children.push({
-                    name: row['Computational Concepts'],
-                    children: []
-                })
-            }
-            const cat2 = cat.children.find(a => a.name === row['Computational Concepts']);
-            if (!cat2.children.find(a => a.name === row['CS Category 3 Lesson'])) {
-                cat2.children.push({
-                    name: row['CS Category 3 Lesson'],
-                    children: []
-                    // value: 1,
-                })
-            }
-        
-            const cat3 = cat2.children.find(a => a.name === row['CS Category 3 Lesson']);
-            if (!cat3.children.find(a => a.name === row['Learning Objectives'])) {
-                let val = 1;
-                cat3.children.push({
-                    name: row['Learning Objectives'],
-                    value: val
-                })
-            }
-
-        }
-
-
-        // Tony's value adjuster depending on string length 
-
-        // All of below is to set the value according to length of LO string
-
-// First find the max level
-let max = 0;
-for (const lv1 of data.children) {
-    for (const lv2 of lv1.children) {
-      for (const lv3 of lv2.children) {
-        if (lv3.children.length > max) {
-            max = lv3.children.length;
-        }
-    }
-    }
-}
-
-for (const lv1 of data.children) {
-    for (const lv2 of lv1.children) {
-      for (const lv3 of lv2.children) {
-        const num = lv3.children.length;
-        const minWeight = Math.round(max/num);  // This is like the defualt value for the LOs
-        // const minWeight = 10;
-        for (const lv4 of lv3.children) {
-            const lenWeight = Math.round((lv4.name.length)/10);     // This is the length of the LO string
-            const weight = (minWeight > lenWeight) ? minWeight : lenWeight ;            // Set the weigh accordingly
-            lv4.value = weight;
-        }
-    }
-    }
-}
-        
-
-        
-
-        // save data as a json
-        console.log('THIS IS JSON', JSON.stringify(data));
-
-    }
-
-    const changeHandler = (event) => {
-        console.log(event.target.files[0])
-        // Passing file data (event.target.files[0]) to parse using Papa.parse
-        parse(event.target.files[0], {
-          header: true,
-          skipEmptyLines: true,
-          complete: function (results) {
-            // console.log(results.data)
-
-            const data = results.data
-
-            console.log('data', data);
-            csvToJson(data);
-
-
-
-
-          },
+    for (const row of csvData) {
+      // This is the new Category - i.e the uppermost layer
+      // if it is not found in the data object - create that and push it
+      const newCat = data;
+      if (!newCat.children.find((a) => a.name === row["Parent Category"])) {
+        newCat.children.push({
+          name: row["Parent Category"],
+          children: [],
         });
-      };
+      }
 
+      // do same for second layer
+      const cat = newCat.children.find(
+        (a) => a.name === row["Parent Category"]
+      );
+      if (!cat.children.find((a) => a.name === row["Computational Concepts"])) {
+        cat.children.push({
+          name: row["Computational Concepts"],
+          children: [],
+        });
+      }
 
+      // do same for third layer
+      const cat2 = cat.children.find(
+        (a) => a.name === row["Computational Concepts"]
+      );
+      if (!cat2.children.find((a) => a.name === row["CS Category 3 Lesson"])) {
+        cat2.children.push({
+          name: row["CS Category 3 Lesson"],
+          children: [],
+          // value: 1,
+        });
+      }
 
+      // For 4th/final layer
+      const cat3 = cat2.children.find(
+        (a) => a.name === row["CS Category 3 Lesson"]
+      );
+      if (!cat3.children.find((a) => a.name === row["Learning Objectives"])) {
+        let val = 1;
+        cat3.children.push({
+          name: row["Learning Objectives"],
+          value: val,
+        });
+      }
+    }
+
+    // Tony's value adjuster depending on string length
+
+    // All of below is to set the value according to length of LO string
+
+    // First find the max level
+    let max = 0;
+    for (const lv1 of data.children) {
+      for (const lv2 of lv1.children) {
+        for (const lv3 of lv2.children) {
+          if (lv3.children.length > max) {
+            max = lv3.children.length;
+          }
+        }
+      }
+    }
+
+    for (const lv1 of data.children) {
+      for (const lv2 of lv1.children) {
+        for (const lv3 of lv2.children) {
+          const num = lv3.children.length;
+          const minWeight = Math.round(max / num); // This is like the defualt value for the LOs
+          // const minWeight = 10;
+          for (const lv4 of lv3.children) {
+            const lenWeight = Math.round(lv4.name.length / 10); // This is the length of the LO string
+            const weight = minWeight > lenWeight ? minWeight : lenWeight; // Set the weigh accordingly
+            lv4.value = weight;
+          }
+        }
+      }
+    }
+    // save data as a json - THis is done by manually copy and paasting from the console -
+    console.log("THIS IS JSON", JSON.stringify(data));
+  };
+
+  const changeHandler = (event) => {
+    console.log(event.target.files[0]);
+    // Passing file data (event.target.files[0]) to parse using Papa.parse
+    parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        // console.log(results.data)
+
+        const data = results.data;
+
+        console.log("data", data);
+        csvToJson(data); //
+      },
+    });
+  };
 
   return (
     <>
-    <div>DataProcessor</div>
-     
+      <div>DataProcessor</div>
+
       {/* File Uploader */}
       <input
         type="file"
@@ -134,9 +123,6 @@ for (const lv1 of data.children) {
         onChange={changeHandler}
         style={{ display: "block", margin: "10px auto" }}
       />
-
     </>
-    
-
-  )
-}
+  );
+};
--- src/components/Icicle.js
+++ src/components/Icicle.js
@@ -24,8 +24,8 @@ export const Icicle = () => {
 
   var partitionData = (data) => {
     const root = hierarchy(data)
-    .sum((d) => d.value) // sum the values - so can get total value and split
-    .sort((a, b) => b.height - a.height || b.value - a.value); // sort so first one starting at 12:00 is the largest module
+      .sum((d) => d.value) // sum the values - so can get total value and split
+      .sort((a, b) => b.height - a.height || b.value - a.value); // sort so first one starting at 12:00 is the largest module
 
     return partition().size([HEIGHT, ((root.height + 1) * WIDTH) / 3])(root);
   };
@@ -35,25 +35,28 @@ export const Icicle = () => {
     const root = partitionData(data);
     let focus = root;
 
-  const svg = select("svg")
+    const svg = select("svg")
       .attr("viewBox", [0, 0, WIDTH, HEIGHT])
       .style("font", "15px sans-serif")
       .style("width", "50vw")
-      .style("overflow", "auto")
-      // .style("height", "100vh");
+      .style("overflow", "auto");
+    // .style("height", "100vh");
 
-      console.log('svg', svg)
+    console.log("svg", svg);
 
-  const cell = svg
-    .selectAll("g")
-    .data(root.descendants())
-    .join("g")
-      .attr("transform", d => `translate(${d.y0},${d.x0})`);
+    const cell = svg
+      .selectAll("g")
+      .data(root.descendants())
+      .join("g")
+      .attr("transform", (d) => `translate(${d.y0},${d.x0})`);
 
-  const rect = cell.append("rect")
-      .attr("width", d => (d.children) ? d.y1 - d.y0 - 1 : 2 * ( d.y1 - d.y0 - 1))    // The last layer (LOs - no children) is double width so spans to the end when on 2nd to last layer! - use overflow-hidden prop
-      .attr("height", d => rectHeight(d))
-      .attr("fill-opacity", 0.6)              // Target this to change the opacity!
+    const rect = cell
+      .append("rect")
+      .attr("width", (d) =>
+        d.children ? d.y1 - d.y0 - 1 : 2 * (d.y1 - d.y0 - 1)
+      ) // The last layer (LOs - no children) is double width so spans to the end when on 2nd to last layer! - use overflow-hidden prop
+      .attr("height", (d) => rectHeight(d))
+      .attr("fill-opacity", 0.6) // Target this to change the opacity!
       // .attr("fill-opacity", d => {
       //   // console.log('d', d)
       //   if(d.data.name.toLowerCase().includes(searchQuery.toLowerCase())){
@@ -61,9 +64,8 @@ export const Icicle = () => {
       //   }
 
       // })
-      
-      
-      .attr("fill", d => {
+
+      .attr("fill", (d) => {
         if (!d.depth) return "#ccc";
         while (d.depth > 1) d = d.parent;
         return setColor(d.data.name);
@@ -71,87 +73,107 @@ export const Icicle = () => {
       .style("cursor", "pointer")
       .on("click", clicked);
 
-  const text = cell.append("text")
+    const text = cell
+      .append("text")
       .style("user-select", "none")
       .attr("pointer-events", "none")
       .attr("x", 4)
       // .attr("y", 13)
-      .attr("fill-opacity", d => +labelVisible(d))
-      .attr("transform", d => `translate(0,${(Math.max((d.x1 - d.x0)/2 , 14))})`);
+      .attr("fill-opacity", (d) => +labelVisible(d))
+      .attr(
+        "transform",
+        (d) => `translate(0,${Math.max((d.x1 - d.x0) / 2, 14)})`
+      );
 
-  text.append("tspan")
-      .text(d => d.data.name);
+    text.append("tspan").text((d) => d.data.name);
 
-  // const tspan = text.append("tspan")
-  //     .attr("fill-opacity", d => labelVisible(d) * 0.7)
-  //     .text(d => ` ${formatData(d.value)}`);
-  // tSPAN - is the little number
+    // const tspan = text.append("tspan")
+    //     .attr("fill-opacity", d => labelVisible(d) * 0.7)
+    //     .text(d => ` ${formatData(d.value)}`);
+    // tSPAN - is the little number
 
-  cell.append("title")
-      // .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${formatData(d.value)}`);  
-      .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}`); // Remove the value at end of title - easier for tracking parents when highlighting searchQuery
+    cell
+      .append("title")
+      // .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${formatData(d.value)}`);
+      .text(
+        (d) =>
+          `${d
+            .ancestors()
+            .map((d) => d.data.name)
+            .reverse()
+            .join("/")}`
+      ); // Remove the value at end of title - easier for tracking parents when highlighting searchQuery
 
-  function clicked(event, p) {
+    function clicked(event, p) {
+      if (!p.children) {
+        const learningObj = p.data.name; // stored here so can set different opacity
+        setLearningObj(learningObj);
+        console.log("p", p);
+        p = p.parent; // set the reference/target to the parent - so doesn't zoom into LO
+        focus = p;
+      } else {
+        // focus is the box that is expanded to full width
+        // check if the one clicked on is focus or not
+        // if it is focus being clicked - change focus to it's parent so can go backwards
+        // Else change focus to new clicked element
+        console.log("Not a learning objective");
+        focus = focus === p ? (p = p.parent) : p;
+      }
 
-    if(!p.children){
-      const learningObj = p.data.name   // stored here so can set different opacity 
-      setLearningObj(learningObj)
-      console.log('p', p)
-      p = p.parent    // set the reference/target to the parent - so doesn't zoom into LO
-      focus = p 
-    } else{
-      // focus is the box that is expanded to full width 
-    // check if the one clicked on is focus or not 
-    // if it is focus being clicked - change focus to it's parent so can go backwards
-    // Else change focus to new clicked element
-    console.log("Not a learning objective")
-    focus = focus === p ? p = p.parent : p;
+      // console.log('p', p)
+      // console.log('Name', p.data.name)
+      // console.log('p.x0', p.x0)
+      // console.log('p.x1', p.x1)
+      // console.log('p.y0', p.y0)
+      // console.log('p.y1', p.y1)
+
+      root.each(
+        (d) =>
+          (d.target = {
+            // Calculate the fraction of total height you need
+            x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * HEIGHT, //x is a height // Calculate the fraction the current nodes displacement from
+            x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * HEIGHT,
+            // y is going across - NOTE: only translate the in chunks of (WIDTH/3 = 975/3 = 325)
+            y0: d.y0 - p.y0,
+            y1: d.y1 - p.y0, // if clicked on the 2nd to last layer (leave y1)
+          })
+      );
+
+      const t = cell
+        .transition()
+        .duration(750)
+        .attr("transform", (d) => `translate(${d.target.y0},${d.target.x0})`);
+
+      rect
+        .transition(t)
+        .attr("height", (d) => rectHeight(d.target))
+        .attr("fill-opacity", (d) => (d.target.name === learningObj ? 1 : 0.5));
+      text
+        .transition(t)
+        .attr("fill-opacity", (d) => +labelVisible(d.target))
+        .attr(
+          "transform",
+          (d) => `translate(0,${Math.max((d.target.x1 - d.target.x0) / 2, 14)})`
+        ); // centering in y-axis (14 incase very small number)
+
+      // you are translating text but it also has deualt y position - so get rid of that!!!
+      // tspan.transition(t).attr("fill-opacity", d => labelVisible(d.target) * 0.7)
     }
 
-    
-   
-    // console.log('p', p)
-    // console.log('Name', p.data.name)
-    // console.log('p.x0', p.x0)
-    // console.log('p.x1', p.x1)
-    // console.log('p.y0', p.y0)
-    // console.log('p.y1', p.y1)
+    function rectHeight(d) {
+      // Min(bottom - top/2 OR 1)/2
+      // x1-x0 is height
+      // - min(1 OR  x1-x0/2)
+      // console.log('d.x1-d.x0', d.x1-d.x0)
+      // console.log('(d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2 ) ', (d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2 ) ))
+      return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+    }
 
-    root.each(d => d.target = {
-      // Calculate the fraction of total height you need 
-      x0: (d.x0 - p.x0) / (p.x1 - p.x0) * HEIGHT, //x is a height // Calculate the fraction the current nodes displacement from 
-      x1: (d.x1 - p.x0) / (p.x1 - p.x0) * HEIGHT,
-      // y is going across - NOTE: only translate the in chunks of (WIDTH/3 = 975/3 = 325)
-      y0: d.y0 - p.y0,
-      y1: d.y1 - p.y0         // if clicked on the 2nd to last layer (leave y1)
-    });
+    function labelVisible(d) {
+      return d.y1 <= WIDTH && d.y0 >= 0 && d.x1 - d.x0 > 19;
+    }
 
-    
-    const t = cell.transition().duration(750)
-        .attr("transform", d => `translate(${d.target.y0},${d.target.x0})`);
-
-    rect.transition(t).attr("height", d => rectHeight(d.target)).attr("fill-opacity", d => d.target.name === learningObj ? 1 : 0.5)
-    text.transition(t).attr("fill-opacity", d => +labelVisible(d.target))
-    .attr("transform", d => `translate(0,${(Math.max((d.target.x1 - d.target.x0)/2 , 14))})`); // centering in y-axis (14 incase very small number)
-
-    // you are translating text but it also has deualt y position - so get rid of that!!!
-    // tspan.transition(t).attr("fill-opacity", d => labelVisible(d.target) * 0.7)
-  }
-  
-  function rectHeight(d) {
-    // Min(bottom - top/2 OR 1)/2   
-    // x1-x0 is height 
-    // - min(1 OR  x1-x0/2)
-    // console.log('d.x1-d.x0', d.x1-d.x0)
-    // console.log('(d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2 ) ', (d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2 ) ))
-    return (d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2 ) );
-  }
-
-  function labelVisible(d) {
-    return d.y1 <= WIDTH && d.y0 >= 0 && d.x1 - d.x0 > 19;
-  }
-  
-  return svg.node();
+    return svg.node();
   };
 
   // render chart - using the function! - useRef used to do only on first mount - react v18 mounts twice in dev mode!
@@ -161,57 +183,116 @@ export const Icicle = () => {
     effectRan.current = true;
   }, []);
 
-
-  const [learningObj, setLearningObj] = useState("Hello")
+  const [learningObj, setLearningObj] = useState("Hello");
   const [searchQuery, setSearchQuery] = useState("");
 
   const highLightLearningObjectives = () => {
+    console.log("searchQuery", searchQuery);
 
-    if(searchQuery === ""){
-      console.log("Empty Search")
-    } else {
-      console.log('searchQuery', searchQuery)
-
-      console.log("Working")
+    console.log("Working");
     // grab all rectangles - use g and filter using the title!
-    const boxes = document.querySelectorAll("g")
-    // console log them
-    // console.log('boxes', boxes)
-    // console.log('boxes[0].data.name', boxes[2].lastChild.textContent)
-    // Filter them 
-    // const filtered_boxes = boxes.filter((box) => box.lastChild.textContent.includes(`${searchQuery}`))
-    // console.log('filtered_boxes', filtered_boxes)
-    boxes.forEach(box => {
+    const boxes = document.querySelectorAll("g");
 
-      console.log('box', box)
 
-      if(searchQuery === ""){      
-        box.firstChild.style.setProperty("fill-opacity", "0.6", "")
-      } else if (box.lastChild.textContent.toLowerCase().includes(searchQuery.toLowerCase())){
-        // console.log('box', box)
-        // console.log('box.firstChild', box.firstChild)
-        box.firstChild.style.setProperty("fill-opacity", "1", "")
-      } else {
-        box.firstChild.style.setProperty("fill-opacity", "0.6", "")
+    // boxes.forEach((box) => {
+    //   // console.log('box', box)
+
+    //   if (searchQuery === "") {
+    //     box.firstChild.style.setProperty("fill-opacity", "0.6", "");
+    //   } else if (
+    //     box.lastChild.textContent
+    //       .toLowerCase()
+    //       .includes(searchQuery.toLowerCase())
+    //   ) {
+    //     // console.log('box', box)
+    //     // console.log('box.firstChild', box.firstChild)
+    //     box.firstChild.style.setProperty("fill-opacity", "1", "");
+    //   } else {
+    //     box.firstChild.style.setProperty("fill-opacity", "0.6", "");
+    //   }
+    // });
+
+    //     Loop through from end of array (so can grab parents along the way) - don't use foreach
+    console.log("boxes", boxes);
+
+    // initialise HashMap to store ancestor paths so can highlight them aswell
+    const ancestorMap = new Map();
+    for (let i = boxes.length - 1; i >= 0; i--) {
+      const box = boxes[i];
+      const boxDesign = box.children[0];
+      const boxText = box.children[1].textContent;
+      const boxTitle = box.children[2].textContent;
+
+      // console.log("box", box);
+      // console.log("boxDesign", boxDesign);
+      // console.log("boxText", boxText.textContent);
+      // console.log("boxTitle", boxTitle.textContent);
+
+
+      if (searchQuery.trim() === "") {
+        console.log("EMPTY SEARCH QUERY")
+        boxDesign.style.setProperty("fill-opacity", "0.6", "");
+      // If(matches searchQuery)
+      } else if (boxText.toLowerCase().includes(searchQuery.toLowerCase())) {
+        // 	Change opacity
+        console.log("GOT A MATCH")
+        boxDesign.style.setProperty("fill-opacity", "1", "");
+        // 	Also Extract parent/ancestors i.e from the titles - extract, maybe split remove last array index and join again
+        const ancestors = boxTitle.split('/')
+        ancestors.pop();
+        console.log('ancestors', ancestors)
+        // 	Store in a hashMap with the key as the title
+        const ancestorKey = ancestors.join("/") 
+        console.log('ancestorKey', ancestorKey)
+        console.log('ancestorMap', ancestorMap)
+        if(!ancestorMap.has(ancestorKey)){
+          ancestorMap.set(ancestorKey, 1);
+        } else{
+          console.log("Already in the Map!")
+        }
+      } else if (ancestorMap.has(boxTitle)){
+        // Else if (title in ancestors hashMap)
+        // 	ChangeOpacity
+        console.log("This is a parent")
+        boxDesign.style.setProperty("fill-opacity", "1", "");
+        // 	Also extract its parent/ancestors
+
+       // 	Also Extract parent/ancestors i.e from the titles - extract, maybe split remove last array index and join again
+       const ancestors = boxTitle.split('/')
+       ancestors.pop();
+       console.log('ancestors', ancestors)
+       // 	Store in a hashMap with the key as the title
+       const ancestorKey = ancestors.join("/") 
+       console.log('ancestorKey', ancestorKey)
+       console.log('ancestorMap', ancestorMap)
+       if(!ancestorMap.has(ancestorKey)){
+        console.log("Inserting ancestorKey")
+         ancestorMap.set(ancestorKey, 1);
+       } else{
+         console.log("Already in the Map!")
+       }
+        // 	Store in hashMap - so can track all the way upto the top!
+      }else{
+        // 	Just keep opacity as 0.6!
+        boxDesign.style.setProperty("fill-opacity", "0.6", "");
       }
-    })
-    // change the color to white
-    // Now change opacity to increase a bit
+
     }
-    
-  }
+  };
 
   useEffect(() => {
-    highLightLearningObjectives()
-  },[searchQuery])
-
-
+    highLightLearningObjectives();
+  }, [searchQuery]);
 
   return (
-    <div className="icicle-page" style={{display: "flex"}}>
-      <div style={{display: "flex", flexDirection: "column"}}>
+    <div className="icicle-page" style={{ display: "flex" }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
         <h2>Icicle</h2>
-        <input type="text" placeholder="Search" onChange={(e) => setSearchQuery(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
         <svg></svg>
       </div>
       <div>

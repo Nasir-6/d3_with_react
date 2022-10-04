13b8a2207db4e5b90fb27b491566bed8c48dae76
Nasir <nasir-islam@hotmail.com>
2022-10-04 18:29:10 +0100

    refactor highlightSearchQuery

--- src/components/Icicle.js
+++ src/components/Icicle.js
@@ -148,8 +148,6 @@ export const Icicle = () => {
   };
 
 
-
-
   const createAndAppendTitlesToCells = (cells) => {
     return cells
       .append("title")
@@ -218,6 +216,8 @@ export const Icicle = () => {
   }
 
 
+  const [learningObj, setLearningObj] = useState("No learning objectives selected!");
+
   const createChart = () => {
     const root = partitionData(data);
     let currentFocus = root;
@@ -264,82 +264,57 @@ export const Icicle = () => {
   };
 
   // render chart - using the function! - useRef used to do only on first mount - react v18 mounts twice in dev mode!
-  const effectRan = useRef(false);
+  const hasMounted = useRef(false);
   useEffect(() => {
-    if (effectRan.current === false) createChart();
-    effectRan.current = true;
+    if (hasMounted.current === false) createChart();
+    hasMounted.current = true;
   }, []);
 
-  const [learningObj, setLearningObj] = useState("No learning objectives selected!");
   const [searchQuery, setSearchQuery] = useState("");
 
-  const highLightLearningObjectives = () => {
-    
-    // grab all rectangles - use g and filter using the title!
-    const boxes = document.querySelectorAll("g");
-
-    //     Loop through from end of array (so can grab parents along the way) - don't use foreach
-    // console.log("boxes", boxes);
-
-    // initialise HashMap to store ancestor paths so can highlight them aswell
-    const ancestorMap = new Map();
-    for (let i = boxes.length - 1; i >= 0; i--) {
-      const box = boxes[i];
-      const boxDesign = box.children[0];
-      const boxText = box.children[1].textContent;
-      const boxTitle = box.children[2].textContent;
-
-      // console.log("box", box);
-      // console.log("boxDesign", boxDesign);
-      // console.log("boxText", boxText.textContent);
-      // console.log("boxTitle", boxTitle.textContent);
-
-
-      if (searchQuery.trim() === "") {
-        console.log("EMPTY SEARCH QUERY")
-        boxDesign.style.setProperty("fill-opacity", "0.6", "");
-      // If(matches searchQuery)
-      } else if (boxText.toLowerCase().includes(searchQuery.toLowerCase())) {
-        // 	Change opacity
-        console.log("GOT A MATCH")
-        boxDesign.style.setProperty("fill-opacity", "1", "");
-        // 	Also Extract parent/ancestors i.e from the titles - extract, maybe split remove last array index and join again
-        const ancestors = boxTitle.split('/')
+  const storeAncestors = (ancestorMap, boxTitle) =>{
+    const ancestors = boxTitle.split('/')
         ancestors.pop();
-        console.log('ancestors', ancestors)
+
         // 	Store in a hashMap with the key as the title
         const ancestorKey = ancestors.join("/") 
-        console.log('ancestorKey', ancestorKey)
-        console.log('ancestorMap', ancestorMap)
+
         if(!ancestorMap.has(ancestorKey)){
           ancestorMap.set(ancestorKey, 1);
         } else{
           console.log("Already in the Map!")
         }
-      } else if (ancestorMap.has(boxTitle)){
-        // Else if (title in ancestors hashMap)
-        // 	ChangeOpacity
-        console.log("This is a parent")
-        boxDesign.style.setProperty("fill-opacity", "1", "");
-        // 	Also extract its parent/ancestors
+        
+  }
+  const highlightSearchQueries = () => {
+    const boxes = document.querySelectorAll("g");
 
-       // 	Also Extract parent/ancestors i.e from the titles - extract, maybe split remove last array index and join again
-       const ancestors = boxTitle.split('/')
-       ancestors.pop();
-       console.log('ancestors', ancestors)
-       // 	Store in a hashMap with the key as the title
-       const ancestorKey = ancestors.join("/") 
-       console.log('ancestorKey', ancestorKey)
-       console.log('ancestorMap', ancestorMap)
-       if(!ancestorMap.has(ancestorKey)){
-        console.log("Inserting ancestorKey")
-         ancestorMap.set(ancestorKey, 1);
-       } else{
-         console.log("Already in the Map!")
-       }
-        // 	Store in hashMap - so can track all the way upto the top!
+    // initialise HashMap to store ancestor paths so can highlight them aswell
+    const ancestorMap = new Map();
+
+    // Loop through from end of array (so can grab parents along the way) - don't use foreach!
+    for (let i = boxes.length - 1; i >= 0; i--) {
+      const box = boxes[i];
+      const boxDesign = box.children[0];
+      const boxText = box.children[1].textContent; // TODO: TEST IF WORKS NOW WITH WRAP FUNCTION INTRODUCING EXTRA SPANS
+      const boxTitle = box.children[2].textContent;
+
+
+      const matchesQuery = boxText.toLowerCase().includes(searchQuery.toLowerCase().trim());
+      const isAncestorOfAMatch = ancestorMap.has(boxTitle);
+
+      if (searchQuery.trim() === "") {
+        boxDesign.style.setProperty("fill-opacity", "0.6", "");
+
+      } else if (matchesQuery) {
+        boxDesign.style.setProperty("fill-opacity", "1", "");
+        storeAncestors(ancestorMap, boxTitle);
+
+      } else if (isAncestorOfAMatch){
+        boxDesign.style.setProperty("fill-opacity", "1", "");
+        storeAncestors(ancestorMap, boxTitle);
+
       }else{
-        // 	Just keep opacity as 0.6!
         boxDesign.style.setProperty("fill-opacity", "0.6", "");
       }
 
@@ -347,7 +322,7 @@ export const Icicle = () => {
   };
 
   useEffect(() => {
-    highLightLearningObjectives();
+    highlightSearchQueries();
   }, [searchQuery]);
 
   return (

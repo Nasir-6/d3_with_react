38b6a872fb60ae3aec05e8467e8d8204811d919d
Nasir <nasir-islam@hotmail.com>
2022-10-04 19:21:08 +0100

    remove colors import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Face } from './components/pilot/Face';
-import { Colors } from './components/pilot/Colors';
 import { Sunburst } from './components/pilot/Sunburst';
 import { DataProcessor } from './components/data/DataProcessor';
 import { Icicle } from './components/Icicle';

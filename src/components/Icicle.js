5301a56ae99196dbb0c025292d71e9488be73fc2
Nasir <nasir-islam@hotmail.com>
2022-10-04 19:13:27 +0100

    refactor searchBar code

--- src/components/Icicle.js
+++ src/components/Icicle.js
@@ -268,71 +268,12 @@ export const Icicle = () => {
     hasMounted.current = true;
   }, []);
 
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const storeAncestors = (ancestorMap, boxTitle) =>{
-    const ancestors = boxTitle.split('/')
-        ancestors.pop();
-
-        // 	Store in a hashMap with the key as the title
-        const ancestorKey = ancestors.join("/") 
-
-        if(!ancestorMap.has(ancestorKey)){
-          ancestorMap.set(ancestorKey, 1);
-        } else{
-          console.log("Already in the Map!")
-        }
-        
-  }
-  const highlightSearchQueries = () => {
-    const boxes = document.querySelectorAll("g");
-
-    // initialise HashMap to store ancestor paths so can highlight them aswell
-    const ancestorMap = new Map();
-
-    // Loop through from end of array (so can grab parents along the way) - don't use foreach!
-    for (let i = boxes.length - 1; i >= 0; i--) {
-      const box = boxes[i];
-      const boxDesign = box.children[0];
-      const boxText = box.children[1].textContent; // TODO: TEST IF WORKS NOW WITH WRAP FUNCTION INTRODUCING EXTRA SPANS
-      const boxTitle = box.children[2].textContent;
-
-
-      const matchesQuery = boxText.toLowerCase().includes(searchQuery.toLowerCase().trim());
-      const isAncestorOfAMatch = ancestorMap.has(boxTitle);
-
-      if (searchQuery.trim() === "") {
-        boxDesign.style.setProperty("fill-opacity", "0.6", "");
-
-      } else if (matchesQuery) {
-        boxDesign.style.setProperty("fill-opacity", "1", "");
-        storeAncestors(ancestorMap, boxTitle);
-
-      } else if (isAncestorOfAMatch){
-        boxDesign.style.setProperty("fill-opacity", "1", "");
-        storeAncestors(ancestorMap, boxTitle);
-
-      }else{
-        boxDesign.style.setProperty("fill-opacity", "0.6", "");
-      }
-
-    }
-  };
-
-  useEffect(() => {
-    highlightSearchQueries();
-  }, [searchQuery]);
-
+  
   return (
     <div className="icicle-page" style={{ display: "flex" }}>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <h2>Icicle</h2>
         <SearchBar/>
-        <input
-          type="text"
-          placeholder="Search"
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
         <svg></svg>
       </div>
       <div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 export const SearchBar = () => {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const storeAncestors = (ancestorMap, boxTitle) =>{
+      const ancestors = boxTitle.split('/')
+          ancestors.pop();
+  
+          // 	Store in a hashMap with the key as the title
+          const ancestorKey = ancestors.join("/") 
+  
+          if(!ancestorMap.has(ancestorKey)){
+            ancestorMap.set(ancestorKey, 1);
+          } else{
+            console.log("Already in the Map!")
+          }
+          
+    }
+    const highlightSearchQueries = () => {
+      const boxes = document.querySelectorAll("g");
+  
+      // initialise HashMap to store ancestor paths so can highlight them aswell
+      const ancestorMap = new Map();
+  
+      // Loop through from end of array (so can grab parents along the way) - don't use foreach!
+      for (let i = boxes.length - 1; i >= 0; i--) {
+        const box = boxes[i];
+        const boxDesign = box.children[0];
+        const boxText = box.children[1].textContent; // TODO: TEST IF WORKS NOW WITH WRAP FUNCTION INTRODUCING EXTRA SPANS
+        const boxTitle = box.children[2].textContent;
+  
+  
+        const matchesQuery = boxText.toLowerCase().includes(searchQuery.toLowerCase().trim());
+        const isAncestorOfAMatch = ancestorMap.has(boxTitle);
+  
+        if (searchQuery.trim() === "") {
+          boxDesign.style.setProperty("fill-opacity", "0.6", "");
+  
+        } else if (matchesQuery) {
+          boxDesign.style.setProperty("fill-opacity", "1", "");
+          storeAncestors(ancestorMap, boxTitle);
+  
+        } else if (isAncestorOfAMatch){
+          boxDesign.style.setProperty("fill-opacity", "1", "");
+          storeAncestors(ancestorMap, boxTitle);
+  
+        }else{
+          boxDesign.style.setProperty("fill-opacity", "0.6", "");
+        }
+  
+      }
+    };
+  
+    useEffect(() => {
+      highlightSearchQueries();
+    }, [searchQuery]);
+
   return (
-    <div>SearchBar</div>
+    <input
+    type="text"
+    placeholder="Search"
+    onChange={(e) => setSearchQuery(e.target.value)}
+  />
   )
 }
